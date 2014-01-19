9a0ff447aed81f297d933f34c37759ad1342794d
joelongstreet <joelongstreet@gmail.com>
2014-01-19 10:41:35 -0600

    add pin mappings for easy access

---
+++ pin-mapping.js
@@ -0,0 +1,37 @@
+exports.pins = [
+  {
+    pinId : 4,
+    type  : 'head',
+    name  : 'greenHead'
+  },
+  {
+    pinId : 17,
+    type  : 'head',
+    name  : 'yellowHead'
+  },
+  {
+    pinId : 23,
+    type  : 'head',
+    name  : 'redHead'
+  },
+  {
+    pinId : 24,
+    type  : 'head',
+    name  : 'blueHead'
+  },
+  {
+    pinId : 25,
+    type  : 'head',
+    name  : 'orangeHead'
+  },
+  {
+    pinId : 22,
+    type  : 'eye',
+    name  : 'leftEye'
+  },
+  {
+    pinId : 18,
+    type  : 'eye',
+    name  : 'rightEye'
+  }
+];
