0815678f8862b0bfc1472651738a1b417d9855c7
Joe Longstreet <joelongstreet@gmail.com>
2014-01-20 06:34:51 +0000

    make it better

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var say         = require('say');
 var initialize  = require('./libs/initializePins');
 var procedures  = require('./libs/procedures');
-var voices      = require('./libs/voices').voices;
 var phrases     = require('./libs/phrases').phrases;
 var pins        = {};
 
 
 initialize.ready(function(pinMappings){
   var i = 0;
-  var pins = pinMappings;
+  pins = pinMappings;
 
   setTimeout(function(){
     for(var key in pins){
       pins[key].turnOff();
     }
-
+    /*
+    setTimeout(function(){
+      warn();
+    }, 1000);
+    */
     goCrazzzy();
   }, 2000);
 });
@@ -24,28 +27,24 @@ var warn = function(){
   pins.leftEye.turnOn();
   pins.rightEye.turnOn();
 
-  var voice  = voices[Math.floor(Math.random()*voices.length)];
   var phrase = phrases[Math.floor(Math.random()*phrases.length)];
-  say.speak(voice, phrase);
+  say.speak('default', phrase);
 
   setTimeout(function(){
     pins.leftEye.turnOff();
     pins.rightEye.turnOff();
-  }, 5000);
+  }, 8000);
 };
-
 
 var goCrazzzy = function(){
   procedures.setPins(pins);
-  procedures.blinkEyes(300, 5);
+  procedures.eyeJitter(300, 18);
   procedures.pyramid(1000, 3, function(){
     procedures.xHead(500, 1, function(){
       procedures.leftCircle(1000, 1, function(){
         procedures.rightCircle(1000, 1, function(){
-          procedures.eyeJitter(300, 5, function(){
-            procedures.blinkAll(250, 3, function(){
-              console.log('all done');
-            });
+          procedures.blinkAll(250, 3, function(){
+            console.log('all done');
           });
         });
       });
