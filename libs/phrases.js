5a28a6e728a584291514d73cf10535bbb2be7f95
joelongstreet <joelongstreet@gmail.com>
2014-01-20 13:13:59 -0600

    making phrases more computer readable

--- libs/phrases.js
+++ libs/phrases.js
@@ -1,17 +1,36 @@
-exports.phrases = [
+var negativePhrases = [
   'Believe in yourself, cause the rest of us think you\'re an idiot',
-  'There\'s nothing to do at this point except give up',
-  'Quit crying. Cry baby',
-  'The next bug is coming out of your paycheck',
-  'Keep adding to the todo list',
-  'Herp derp the herp derp',
-  'Give up and use tables',
+  'There\'s nothing to do at this point, except give up',
+  'Quit crying, Cry baby, cry cry cry baby',
+  'That\'s it, the next bug is coming out of your paycheck',
+  'Don\'t worry, you\'ll never get this project done',
+  'Herp derp the herp derp, herpin derpin herp herp, fart in your face',
   'Hashtag sigh',
-  'Freals dog, the tests better pass this time',
+  'For real dog, the tests better pass this time',
   'You\'re not using that code anyways, just delete it.',
-  'Another pile of fail',
-  'a s d f a s d f a s d f a s d f a s d f',
-  'By works you must mean. quote doesn\'t work. endquote',
-  'my god, what have you done?',
-  'Wheeeeeeeeeeee'
+  'Yet another pile of fail from you',
+  'a s d f a s d f a s d f a s d f a s d f, just type that',
+  'By works you must mean. quote doesn\'t work. endquote, jackass',
+  'my god, what have you done!',
+  'Wheeeeeeeeeeee!',
 ];
+
+var positivePhrases = [
+  'Yahoo',
+  'You go get em tiger',
+  'That, a boy',
+  'Good job sport',
+  'High fives all around',
+  'Good job, but, I can\'t believe it took you this long'
+];
+
+var randomNegative = function(){
+  return negativePhrases[Math.floor(Math.random()*negativePhrases.length)];
+};
+
+var randomPositive = function(){
+  return positivePhrases[Math.floor(Math.random()*positivePhrases.length)];
+};
+
+exports.randomNegative = randomNegative;
+exports.randomPositive = randomPositive;
